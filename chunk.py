7923be71e43992fe083d1c75b79a4c3f2d145716
bobo-baggins <Urbanecoguard@gmail.com>
2023-10-08 16:19:59 -0400

    Added text_Box class to chunky.py and some implementation to main.py

--- chunk.py
+++ chunk.py
@@ -9,8 +9,35 @@ class Chunk:
 	def draw(self, frame):
 		print("poo")
 
-class Words(Chunk):
+class text_B(Chunk):
 	def draw(self, frame):
-		self.lab = tk.Label(master=frame, text=self.data)
-		self.lab.pack()
+	# Creates title label using "data" and an empty multi-line text entry box
+		self.title = tk.Label(
+			master=frame,
+			text=self.data,
+			height = 5,
+			width = 25)
 
+		self.text_box = tk.Text(
+			master=frame,
+			height = 25,
+			width = 125)
+
+		self.title.pack()
+		self.text_box.pack(fill=tk.BOTH, expand=True)
+
+	def hide_title(self):	
+		self.title.forget()
+
+	def fill_text(self,cords, words):	
+		self.text_box.insert(str(cords), str(words))
+
+	def lock_text(self, frame):
+		#Grabs text from the text box starting at coloumn at 1 letter 0
+		self.wordss = self.text_box.get("1.0", tk.END)
+		self.text_box.forget()
+		#Puts text grabbed into a label
+		self.text_box_L = tk.Label(master=frame,text=self.wordss)
+		self.text_box_L.pack()
+
+		
